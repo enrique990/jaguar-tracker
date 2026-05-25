83ce965eb1f67e7e6ca926b32493670fe1b7a6c2
Enrique990 <voidenrique@gmail.com>
2026-05-25 17:50:38 -0600

    chore: inicializar plantilla base de la API con Spring Boot y dependencias

---
+++ Jaguar_api/src/main/java/ni/edu/uam/Jaguar_api/JaguarApiApplication.java
@@ -0,0 +1,13 @@
+package ni.edu.uam.Jaguar_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JaguarApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JaguarApiApplication.class, args);
+	}
+
+}
---
+++ Jaguar_api/src/test/java/ni/edu/uam/Jaguar_api/JaguarApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ni.edu.uam.Jaguar_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JaguarApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
